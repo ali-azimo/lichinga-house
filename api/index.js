0c059deab2545a30f8bd5365806e4af37a1be0da
Ali Azimo <webdesign.aejl@gmail.com>
2026-03-18 12:29:33 +0200

    em andamento

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+
 // Rotas
 import userRouter from './routes/user.route.js';
 import cadRouter from './routes/cad.route.js';
